2ab5fcb563fa37842aff5b07defd1fe5de354795
Juan Rodriguez <jrodri89@uwo.ca>
2020-11-29 19:50:01 -0500

    Basic database interface endpoint

--- assignment4/backend/app.js
+++ assignment4/backend/app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+var mysql = require('mysql')
 
+const app = express();
 
-const app = express.app();
+const port = process.env.port || 3000;
 
-app.listen(5000);
+app.get('/', (req, res) => {
+    var connection = mysql.createConnection({
+      host: 'project-3309.c0vk0iwpo9it.us-east-2.rds.amazonaws.com',
+      user: 'admin',
+      password: 'pa$$word',
+      database: '3309'
+    })
 
+    connection.connect()
+
+    connection.query('SELECT * FROM Student', function (err, rows, fields) {
+      if (err) throw err
+
+        console.log('Student is:', rows[0])
+    })
+
+    connection.end()
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
 
